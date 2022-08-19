017ca008c27915b94560869c9075643940d59da8
Your Name <urielparavi75@gmail.com>
2022-08-19 20:31:20 +0300

    aaa

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,7 +5,6 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
-// const { check, validationResult } = require('express-validator/check'); // Incase we have an error
 
 const User = require('../../models/User');
 

e16143558b1df0238ea8ec49f5ace314c70ef5c5
Your Name <urielparavi75@gmail.com>
2022-08-25 10:38:18 +0300

    aaa

---
+++ prc.js
@@ -0,0 +1,10 @@
+       /*  "client": "npm start --prefix client",
+"dev": "concurrently \"npm run server\" \"npm run client\"" */
+
+
+    // "scripts": {
+    //   "start": "node server",
+    //   "server": "nodemon server",
+    //   "client": "npm start --prefix client",
+    //   "dev": "concurrently \"npm run server\" \cd ../cd frontend/cd client && "npm run client\""   => to start the backend and the frontend both
+    // },
